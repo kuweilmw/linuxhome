1550f590f31a9cb0cc7464b9afc7e26d1ed5fd2c
kuweilmw <lmw2330359061@163.com>
2019-10-21 23:29:51 -0700

    linuxprogram week7

---
+++ linuxprogram/week7/code/forktest1.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+
+ 
+
+char buf[] = {"write to stdout\n"};
+
+int main()
+
+{
+
+	pid_t id;
+
+ 
+
+	if(write(STDOUT_FILENO, buf, sizeof(buf) - 1) != sizeof(buf) - 1)
+
+	perror("can't write");
+
+	printf("printf\n");
+
+	id = fork();
+
+	if(id < 0)
+
+	perror("fork error");
+
+ 
+
+	exit(0);
+
+}
